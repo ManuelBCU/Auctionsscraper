3260e78bac3615765401d4ab404714c962679740
ManuelBCU <43589178+ManuelBCU@users.noreply.github.com>
2019-02-26 11:38:45 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,15 +1,11 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+html = scraperwiki.scrape("https://www.sdlauctions.co.uk/property-list/")
+
+root = lxml.html.fromstring(html)
+print(root)
+#root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
